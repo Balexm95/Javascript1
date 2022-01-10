3f6436d8e5c9fe3f408671378e221197a11ceeb1
Balexm95 <bria.mccoy95@gmail.com>
2022-01-10 16:27:42 -0500

    main

---
+++ easy.js
@@ -0,0 +1,8 @@
+//EASY
+let nameJohn = 'John';
+let nameThomas = 'Thomas';
+let numJohn = 4;
+let numThomas = 6;
+let nameDiff = 2;
+
+console.log('The name ' + nameThomas + ' is longer than ' + nameJohn +' by ' + nameDiff +' characters');
---
+++ hard.js
@@ -0,0 +1,24 @@
+//HARD
+let num1 = prompt('Enter number 1');
+
+let num2 = prompt('Enter number 2');
+
+function add(num1, num2){
+    let result= (num1+num2); 
+    console.log(result);
+}
+
+function subtract(num1, num2){
+   let result=(num1 - num2);
+    console.log(result);
+}
+
+function multiply(num1, num2){
+   let result=(num1 * num2);
+    console.log(result);
+}
+
+function divide(num1, num2){
+     let result=(num1 / num2);
+     console.log(result);
+}
